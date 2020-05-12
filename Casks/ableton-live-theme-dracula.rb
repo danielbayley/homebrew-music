a65454af537f6b0ab766ded6be4ec3180e2decba
Daniel Bayley <daniel.bayley@me.com>
2020-05-12 18:48:05 +0100

    Add Ableton Live Dracula theme

---
+++ Casks/ableton-live-theme-dracula.rb
@@ -0,0 +1,19 @@
+cask 'ableton-live-theme-dracula' do
+  version :latest
+  sha256 :no_check
+
+  # github.com/dracula/ableton-live was verified as official when first introduced to the cask
+  url 'https://github.com/dracula/ableton-live/archive/master.zip'
+  name 'Dracula'
+  homepage 'https://draculatheme.com/ableton-live'
+
+  theme = "ableton-live-master/#{name.first}.ask"
+  app, = Dir["#{appdir}/Ableton Live*.app"]
+
+  if app && File.directory?(app)
+    artifact theme, target: "#{app}/Contents/App-Resources/Themes/#{name.first}.ask"
+  else
+    installer manual: theme
+    uninstall {}
+  end
+end
