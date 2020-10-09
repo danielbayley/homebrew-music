5b66027f3330165e24dfe427591b9f490991b805
Daniel Bayley <daniel.bayley@me.com>
2020-10-09 20:18:00 +0100

    Fix Cask style checks

--- Casks/ableton-live-theme-dracula.rb
+++ Casks/ableton-live-theme-dracula.rb
@@ -1,11 +1,12 @@
-cask 'ableton-live-theme-dracula' do
+cask "ableton-live-theme-dracula" do
   version :latest
   sha256 :no_check
 
-  # github.com/dracula/ableton-live was verified as official when first introduced to the cask
-  url 'https://github.com/dracula/ableton-live/archive/master.zip'
-  name 'Dracula'
-  homepage 'https://draculatheme.com/ableton-live'
+  # github.com/dracula/ableton-live/ was verified as official when first introduced to the cask
+  url "https://github.com/dracula/ableton-live/archive/master.zip"
+  name "Dracula"
+  desc "Dark theme for Ableton Live"
+  homepage "https://draculatheme.com/ableton-live"
 
   theme = "ableton-live-master/#{name.first}.ask"
   app, = Dir["#{appdir}/Ableton Live*.app"]
--- Casks/mixed-in-key.rb
+++ Casks/mixed-in-key.rb
@@ -1,27 +1,28 @@
-cask 'mixed-in-key' do
-  version '8.5.3.4078'
-  sha256 '5391b67d484ab3b7e377ac137a19c0da7eb8bd2298326810768fdb62f81ba82a'
+cask "mixed-in-key" do
+  version "8.5.3.4078"
+  sha256 "5391b67d484ab3b7e377ac137a19c0da7eb8bd2298326810768fdb62f81ba82a"
 
-  envar = "HOMEBREW_#{token.upcase.tr '-', '_'}_VIP_CODE"
+  envar = "HOMEBREW_#{token.upcase.tr "-", "_"}_VIP_CODE"
   url "https://builds.mixedinkey.com/download/39/release/latest?key=#{ENV[envar]}"
-  appcast 'https://community.mixedinkey.com/Topics/24385/mixed-in-key-changelogs',
+  appcast "https://community.mixedinkey.com/Topics/24385/mixed-in-key-changelogs",
           must_contain: "#{version.major_minor_patch} (Mac)"
-  name 'Mixed In Key'
-  homepage 'http://mixedinkey.com/'
+  name "Mixed In Key"
+  desc "Harmonic mixing for DJs and music producers"
+  homepage "https://mixedinkey.com/"
 
   auto_updates true
 
   app "Mixed In Key #{version.major}.app"
 
-  bundle_id = 'com.mixedinkey.application'
+  bundle_id = "com.mixedinkey.application"
   uninstall quit: bundle_id
 
   zap trash: [
-               "~/Library/*/#{bundle_id}",
-               '~/Library/Application Support/Mixedinkey',
-               "~/Library/Preferences/#{bundle_id}.plist",
-               "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/#{bundle_id}.sfl*",
-             ]
+    "~/Library/*/#{bundle_id}",
+    "~/Library/Application Support/Mixedinkey",
+    "~/Library/Preferences/#{bundle_id}.plist",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/#{bundle_id}.sfl*",
+  ]
 
   caveats "Cask requires a #{envar} environment variable to be set with your VIP code."
 end
