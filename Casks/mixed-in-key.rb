e9638bb225fdfb01eb3ff9b0381356ea3bc67c89
Daniel Bayley <daniel.bayley@me.com>
2020-11-19 19:35:16 +0000

    Update Mixed In Key

--- Casks/mixed-in-key.rb
+++ Casks/mixed-in-key.rb
@@ -2,9 +2,10 @@ cask "mixed-in-key" do
   version "8.5.3.4078"
   sha256 "5391b67d484ab3b7e377ac137a19c0da7eb8bd2298326810768fdb62f81ba82a"
 
-  envar = "HOMEBREW_#{token.upcase.tr "-", "_"}_VIP_CODE"
-  url "https://builds.mixedinkey.com/download/39/release/latest?key=#{ENV[envar]}"
-  appcast "https://community.mixedinkey.com/Topics/24385/mixed-in-key-changelogs",
+  vip_code = `security find-generic-password -ws #{token}`
+
+  url "https://builds.mixedinkey.com/download/39/release/latest?key=#{vip_code.chomp}"
+  appcast "https://community.mixedinkey.com/Topics/24385/#{token}-changelogs",
           must_contain: "#{version.major_minor_patch} (Mac)"
   name "Mixed In Key"
   desc "Harmonic mixing for DJs and music producers"
@@ -17,12 +18,17 @@ cask "mixed-in-key" do
   bundle_id = "com.mixedinkey.application"
   uninstall quit: bundle_id
 
+  support = "~/Library/Application Support"
   zap trash: [
-    "~/Library/*/#{bundle_id}",
-    "~/Library/Application Support/Mixedinkey",
-    "~/Library/Preferences/#{bundle_id}.plist",
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/#{bundle_id}.sfl*",
+    "~/Library/*/#{bundle_id}*",
+    "#{support}/Mixedinkey",
+    "#{support}/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/#{bundle_id}.sfl*",
   ]
 
-  caveats "Cask requires a #{envar} environment variable to be set with your VIP code."
+  caveats <<~EOS
+    Cask requires a Keychain entry with with your VIP code.
+
+    To add the Keychain entry:
+      security add-generic-password -T /usr/bin/security -a #{ENV["USER"]} -s #{token} -Uw <vip code>
+  EOS
 end
