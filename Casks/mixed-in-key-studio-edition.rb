7aa5fc7fbab5fb03398c51616948a28a47494818
Daniel Bayley <daniel.bayley@me.com>
2022-08-23 21:31:08 +0100

    Update Mixed in Key Studio Edition to 1.2.1

--- Casks/mixed-in-key-studio-edition.rb
+++ Casks/mixed-in-key-studio-edition.rb
@@ -1,14 +1,18 @@
 cask "mixed-in-key-studio-edition" do
-  version "1.2.0.183"
-  sha256 "1bb1d6aa10158faf2e951f5f85de4a9f46412bdcb8c6eda00f603bf665cf611e"
+  version "1.2.1.221"
+  sha256 :no_check
 
-  url "https://builds.mixedinkey.com/download/45/release/latest?key=dh-745a7ef1648d471e82893bc23806cadc"
-  appcast "https://community.mixedinkey.com/Topics/24995/#{token}-changelogs",
-          must_contain: "MIK SE #{version.major}.1 (Mac)"
+  key = "dh-745a7ef1648d471e82893bc23806cadc"
+  url "https://builds.mixedinkey.com/download/45/release/latest?key=#{key}"
   name token.titleize
   name "Mixed In Key SE"
   desc "Key detection for your DAW"
   homepage "https://mixedinkey.com/studio-edition"
+
+  livecheck do
+    url :url
+    strategy :header_match
+  end
 
   auto_updates true
 
